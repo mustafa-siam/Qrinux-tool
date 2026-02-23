8fa32fc4025689d36eb37a9b0e5b0ab7f1a12215
mustafa-siam <mustafasiam357@gmail.com>
2026-02-23 12:16:54 +0600

    third commit

---
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+
+"use client"; 
+
+import React from "react";
+
+export default function dashboardPage() {
+  return (
+    <div>
+      <h1>Analytics Dashboard</h1>
+      <p>Stats will appear here.</p>
+    </div>
+  );
+}
---
+++ src/components/PageComponents/Dashboard/LinkTable.tsx
@@ -0,0 +1,13 @@
+// src/app/(dashboard)/analytics/page.tsx
+"use client"; // only if you use client-side hooks
+
+import React from "react";
+
+export default function LinkTable() {
+  return (
+    <div>
+      <h1>Analytics Dashboard</h1>
+      <p>Stats will appear here.</p>
+    </div>
+  );
+}
